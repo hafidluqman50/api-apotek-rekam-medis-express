58df8c10c14988b1dfdd382dcbd3433b53cda414
hafidluqman50 <hafidluqmanulhakim@gmail.com>
2023-10-13 03:14:02 +0800

    refac: refactoring all routing(2)

--- routes/router.js
+++ routes/router.js
@@ -1,5 +1,10 @@
-
+import express from 'express'
+const baseRouter = express.Router()
 
 // const router = express.Router()
-
+baseRouter.get('/',(req,res) => {
+    res.status(200).send({message:'API Apotek Rekam Medis'})
+})
 // export {router}
+
+export {baseRouter}
