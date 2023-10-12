fa7b97125eb9944dc788ae78b641e800e4d8f440
hafidluqman50 <hafidluqmanulhakim@gmail.com>
2023-10-13 02:54:21 +0800

    refac(admin-routes): refac admin routing controllers

--- controllers/obat.controller.js
+++ controllers/obat.controller.js
@@ -1,36 +1,66 @@
 import Obat from '../models/obat.model.js'
 
-const getObat = async() => {
-    return await Obat.findAll()
-}
-
-const createObat = async(request) => {
-    return await Obat.create({
-        nama_obat:request.nama_obat,
-        jenis_obat:request.jenis_obat,
-        stok_obat:request.stok_obat,
+const getObat = (req,res) => {
+    Obat.findAll()
+    .then((result) => {
+        res.status(200).send({status:true,message:'Berhasil Get Data Obat',response:result})
+    })
+    .catch((error) => {
+        res.status(400).send({status:false,message:'Error',response:error.message})
     })
 }
 
-const detailObat = async(id) => {
-    return await Obat.findByPk(id)
+const createObat = (req,res) => {
+    Obat.create({
+        nama_obat:req.body.nama_obat,
+        jenis_obat:req.body.jenis_obat,
+        stok_obat:req.body.stok_obat,
+    })
+    .then((result) => {
+        res.status(200).send({status:true,message:'Berhasil Insert Obat',response:req.body})
+    })
+    .catch((error) => {
+        res.status(400).send({status:false,message:'Error!',response:error})
+    })
 }
 
-const updateObat = async(id,request) => {
-    return await Obat.update({
-        nama_obat:request.nama_obat,
-        jenis_obat:request.jenis_obat,
-        stok_obat:request.stok_obat,
+const detailObat = (req,res) => {
+    Obat.findByPk(req.params.id)
+    .then((result) => {
+        res.status(200).send({status:true,message:'Berhasil Get Data Obat Detail!',response:result})
+    })
+    .catch((error) => {
+        res.status(400).send({status:false,message:'Error!',response:error})
+    })
+}
+
+const updateObat = (req,res) => {
+    Obat.update({
+        nama_obat:req.body.nama_obat,
+        jenis_obat:req.body.jenis_obat,
+        stok_obat:req.body.stok_obat,
     },{where:{
-        id
+        id:req.params.id
     }})
+    .then((result) => {
+        res.status(200).send({status:true, message:'Berhasil Update Obat!', response:result})
+    })
+    .catch((error) => {
+        res.status(400).send({status:false,message:'Error!',response:error})
+    })
 }
 
-const deleteObat = async(id) => {
-    return await Obat.destroy({
+const deleteObat = (req,res) => {
+    Obat.destroy({
         where:{
-            id
+            id:req.params.id
         }
+    })
+    .then((result) => {
+        res.status(200).send({status:true, message:'Berhasil Delete Obat!', response:result})
+    })
+    .catch((result)=> {
+        res.status(400).send({status:false,message:'Error!',response:error})
     })
 }
 
--- routes/administrator/obat.router.js
+++ routes/administrator/obat.router.js
@@ -5,45 +5,14 @@ import * as ObatController from '../../controllers/obat.controller.js'
 const obatRouterAdmin = express.Router();
 const multParse       = multer();
 
-obatRouterAdmin.get('/',(req,res) => {
-    // console.log(ObatController.getObat())
-    ObatController.getObat().then((result) => {
-        res.status(200).send({status:true,message:'Berhasil Get Data Obat',response:result})
-    }).catch((error) => {
-        res.status(400).send({status:false,message:'Error',response:error.message})
-    })
-})
+obatRouterAdmin.get('/',ObatController.getObat)
 
-obatRouterAdmin.post('/create',multParse.none(),(req,res) => {
-    ObatController.createObat(req.body).then((result) => {
-        res.status(200).send({status:true,message:'Berhasil Insert Obat',response:req.body})
-    }).catch((error) => {
-        res.status(400).send({status:false,message:'Error!',response:error})
-    })
-})    
+obatRouterAdmin.post('/create',multParse.none(),ObatController.createObat)    
 
-obatRouterAdmin.get('/detail/:id',(req,res) => {
-    ObatController.detailObat(req.params.id).then((result) => {
-        res.status(200).send({status:true,message:'Berhasil Get Data Obat Detail!',response:result})
-    }).catch((error) => {
-        res.status(400).send({status:false,message:'Error!',response:error})
-    })
-})
+obatRouterAdmin.get('/detail/:id',ObatController.detailObat)
 
-obatRouterAdmin.put('/update/:id',(req,res) => {
-    ObatController.updateObat(req.params.id,req.body).then((result) => {
-        res.status(200).send({status:true, message:'Berhasil Update Obat!', response:result})
-    }).catch((error) => {
-        res.status(400).send({status:false,message:'Error!',response:error})
-    })
-})
+obatRouterAdmin.put('/update/:id',ObatController.updateObat)
 
-obatRouterAdmin.delete('/delete/:id',(req,res) => {
-    ObatController.deleteObat(req.params.id).then((result) => {
-        res.status(200).send({status:true, message:'Berhasil Delete Obat!', response:result})
-    }).catch((error) => {
-        res.status(400).send({status:false,message:'Error!',response:error})
-    })
-})
+obatRouterAdmin.delete('/delete/:id',ObatController.deleteObat)
 
 export default obatRouterAdmin
